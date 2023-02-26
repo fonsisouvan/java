7d55925b138af242b13c06543b79333b7fc1eb26
fon <sisouvanfon@gmail.com>
2023-02-26 20:45:31 +0700

    bai tap ishape

--- src/_interface/ShowShape.java
+++ src/_interface/ShowShape.java
@@ -2,11 +2,9 @@ package _interface;
 
 public class ShowShape {
     public static void main(String[] args){
-        IShape c1 = new Circle(3);
-        IShape c2 = new Circle(4);
-        IShape r1 = new Recttangle(3,4);
-        IShape r2 = new Recttangle(4,5);
-        IShape[] list = {c1,c2,r1,r2};
+        IShape c1 = new Circle(6);
+        IShape r1 = new Recttangle(5,6);
+        IShape[] list = {c1,r1};
         for (IShape s:list){
 
             System.out.println(s.getArea()+"\n"+s.getPerimeter());
